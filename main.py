7db8073f31efe49f49cb34f7fee3aea17eb5d880
snizhana-m <160052868+banmat@users.noreply.github.com>
2026-06-23 22:21:32 +0300

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def multiply_numbers(a, b):
 
 def hash_password(password):
     # Увага: MD5 вважається криптографічно небезпечним!
-    hashed = hashlib.md5(password.encode()).hexdigest()
-    return hashed
+    hashed = hashlib.sha256(password.encode()).hexdigest()    return hashed
 
 if __name__ == "__main__":
     print(multiply_numbers(5, 5))
