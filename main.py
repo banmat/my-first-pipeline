c3385b42a1505404d2931007ad571f9b0c975f8a
snizhana-m <160052868+banmat@users.noreply.github.com>
2026-06-23 22:47:14 +0300

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def multiply_numbers(a, b):
     return a * b
 
 def hash_password(password):
-    # Увага: MD5 - це слабкий алгоритм, який Bandit має знайти!
-    hashed = hashlib.md5(password.encode()).hexdigest()
+    # Тепер використовуємо надійний алгоритм
+    hashed = hashlib.sha256(password.encode()).hexdigest()
     return hashed
 
 if __name__ == "__main__":
