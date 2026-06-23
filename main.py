59d5e1aa9163c9a481dbcb8d6afbd35a336dd48f
snizhana-m <160052868+banmat@users.noreply.github.com>
2026-06-23 22:00:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def multiply_numbers(a, b):
+    """Функція, яка перемножує два числа."""
+    return a * b
+
+
+if __name__ == "__main__":
+    result = multiply_numbers(5, 5)
+    print(f"Результат: {result}")
